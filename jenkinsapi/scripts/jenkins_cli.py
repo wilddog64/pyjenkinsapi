e402fd38b9ffe05d96ea87cb3d3404be235b106c
Chengkai Liang <ckm.liang@gmail.com>
2017-05-23 09:48:52 -0700

    add helper functions for chaining

--- jenkinsapi/scripts/jenkins_cli.py
+++ jenkinsapi/scripts/jenkins_cli.py
@@ -1,6 +1,7 @@
 import click
 import jenkinsapi.core
 from jenkinsapi.config.core import config_section_map
+from functools import update_wrapper
 
 @click.group(chain=True)
 @click.option('-c', '--config-path', help='jenkins-cli configuration path', default='')
@@ -38,6 +39,48 @@ def views(jenkins, all, jobs, names):
 @jenkins.command('jobs')
 def jobs():
     click.echo('list all jobs from a given jenkins servers')
+
+@cli.resultcallback()
+def process_commands(processors):
+    """This result callback is invoked with an iterable of all the chained
+    subcommands.  As in this example each subcommand returns a function
+    we can chain them together to feed one into the other, similar to how
+    a pipe on unix works.
+    """
+    # Start with an empty iterable.
+    stream = ()
+
+    # Pipe it through all stream processors.
+    for processor in processors:
+        stream = processor(stream)
+
+    # Evaluate the stream and throw away the items.
+    for _ in stream:
+        pass
+
+
+def processor(f):
+    """Helper decorator to rewrite a function so that it returns another
+    function from it.
+    """
+    def new_func(*args, **kwargs):
+        def processor(stream):
+            return f(stream, *args, **kwargs)
+        return processor
+    return update_wrapper(new_func, f)
+
+
+def generator(f):
+    """Similar to the :func:`processor` but passes through old values
+    unchanged and does not pass through the values as parameter.
+    """
+    @processor
+    def new_func(stream, *args, **kwargs):
+        for item in stream:
+            yield item
+        for item in f(*args, **kwargs):
+            yield item
+    return update_wrapper(new_func, f)
 
 if __name__ == '__main__':
     jenkins()
