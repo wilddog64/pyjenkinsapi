7a350a5c5d4aea6e08cf1515ac765963c45b0705
chengkai liang <chengkai.liang@costcotravel.com>
2023-06-09 08:22:43 -0700

    make cli aware of environment variables

--- jenkinsapi/scripts/jenkins_cli.py
+++ jenkinsapi/scripts/jenkins_cli.py
@@ -6,18 +6,28 @@ from jenkinsapi.config.core import config_section_map
 
 @click.group()
 @click.option('-c', '--config-path', help='jenkins-cli configuration path', default='')
-@click.option('--section-name', help='section in jenkins-cli configuration file', default='lcjenkins')
+@click.option('--section-name', help='section in jenkins-cli configuration file', default='')
 @click.option('-s', '--jenkins-server-url', help='jenkins server url', default='')
 @click.option('-u', '--jenkins-user', help='jenkins user name', default='')
 @click.option('-p', '--jenkins-password', help='jenkins user password', default='')
 @click.pass_context
 def jenkins(ctx, jenkins_server_url, config_path, section_name, jenkins_user, jenkins_password):
-    jenkins_config = config_section_map(config_file='jenkins.ini',
-                                        config_file_path=config_path,
-                                        section_name='lcjenkins')
-    jenkins_user = jenkins_config['user'] if jenkins_user == '' else jenkins_user
-    jenkins_password = jenkins_config['password'] if jenkins_password == '' else jenkins_password
-    jenkins_server_url = jenkins_config['url'] if jenkins_server_url == '' else jenkins_server_url
+    jenkins_config = None
+    if not section_name == '':
+        jenkins_config = config_section_map(config_file='jenkins.ini',
+                                            config_file_path=config_path,
+                                            section_name='lcjenkins')
+    # let's see if there're any environment variables defined
+    environ = os.environ
+    jenkins_user = environ['JENKINS_USER'] if 'JENKINS_USER' in environ else ''
+    jenkins_password = environ['JENKINS_API_TOKEN'] if 'JENKINS_API_TOKEN' in environ else ''
+    jenkins_server_url = environ['JENKINS_URL'] if 'JENKINS_URL' in environ else ''
+
+    if jenkins_config:
+        jenkins_user = jenkins_config['user'] if jenkins_user == '' else jenkins_user
+        jenkins_password = jenkins_config['password'] if jenkins_password == '' else jenkins_password
+        jenkins_server_url = jenkins_config['url'] if jenkins_server_url == '' else jenkins_server_url
+
     ctx.obj = jenkinsapi.core.Jenkins(jenkins_server_url, jenkins_user, jenkins_password)
 
 @jenkins.command('views')
